c0c02197352af362fc6589fe181bd433230340e5
wkl--007 <499657357@qq.com>
2018-05-24 15:28:35 +0800

    动态设置title

--- src/layouts/basicLayout.js
+++ src/layouts/basicLayout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link, Route, Switch, Redirect } from 'react-router-dom'
-
+import DocumentTitle from 'react-document-title'
+import pathToRegexp from 'path-to-regexp'
 import { getMenuData } from '../router/menu'
 import { getRoutes } from '../utils/utils'
 
@@ -22,36 +23,54 @@ const getRedirect = item => {
 getMenuData().forEach(getRedirect)
 
 class basicLayout extends Component {
+  //获取页面标题
+  getPageTitle = () => {
+    let {routerData, location} = this.props
+    let {pathname} = location
+    let title = '小王管理系统'
+    let currentRouterData = null
+    Object.keys(routerData).forEach(key => {
+      if (pathToRegexp(key).test(pathname)) {
+        currentRouterData = routerData[key]
+      }
+    })
+    if (currentRouterData && currentRouterData.name) {
+      title = `${currentRouterData.name} - ${title}`
+    }
+    return title
+  }
 
   render () {
     let {match, routerData} = this.props
     let routes = getRoutes(match.path, routerData)
     let bashRedirect = routes[0].path
     return (
-      <div id="basic-layout">
-        <Switch>
-          {
-            redirectData.map(item => {
-              return <Redirect
-                exact
-                key={item.from}
-                from={item.from}
-                to={item.to}/>
-            })
-          }
-          {
-            routes.map(item => {
-              return <Route
-                key={item.key}
-                path={item.path}
-                component={item.component}
-                exact={item.exact}
-              />
-            })
-          }
-          <Redirect exact from="/" to={bashRedirect}/>
-        </Switch>
-      </div>
+      <DocumentTitle title={this.getPageTitle()}>
+        <div id="basic-layout">
+          <Switch>
+            {
+              redirectData.map(item => {
+                return <Redirect
+                  exact
+                  key={item.from}
+                  from={item.from}
+                  to={item.to}/>
+              })
+            }
+            {
+              routes.map(item => {
+                return <Route
+                  key={item.key}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
+              })
+            }
+            <Redirect exact from="/" to={bashRedirect}/>
+          </Switch>
+        </div>
+      </DocumentTitle>
     )
   }
 }
--- src/layouts/userLayout.js
+++ src/layouts/userLayout.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react'
 import { Link, Route, Switch, Redirect } from 'react-router-dom'
+import DocumentTitle from 'react-document-title'
 
 import { getRoutes } from '../utils/utils'
 
 class UserLayout extends Component {
+  //获取页面标题
+  getPageTitle = () => {
+    let {routerData, location} = this.props
+    let {pathname} = location
+    let title = '小王管理系统'
+    if (routerData[pathname] && routerData[pathname].name) {
+      title = `${routerData[pathname].name} - ${title}`
+    }
+    return title
+  }
+
   render () {
-    let {match,routerData} = this.props
+    let {match, routerData} = this.props
     let routes = getRoutes(match.path, routerData)
     return (
-      <div id="user-layout">
-        <Switch>
-          {
-            routes.map(item => {
-              return <Route
-                key={item.key}
-                path={item.path}
-                component={item.component}
-                exact={item.exact}
-              />
-            })
-          }
-          <Redirect exact from="/user" to="/user/login"/>
-        </Switch>
-      </div>
+      <DocumentTitle title={this.getPageTitle()}>
+        <div id="user-layout">
+          <Switch>
+            {
+              routes.map(item => {
+                return <Route
+                  key={item.key}
+                  path={item.path}
+                  component={item.component}
+                  exact={item.exact}
+                />
+              })
+            }
+            <Redirect exact from="/user" to="/user/login"/>
+          </Switch>
+        </div>
+      </DocumentTitle>
     )
   }
 }
