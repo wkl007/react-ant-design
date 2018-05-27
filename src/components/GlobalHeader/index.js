41f6c96eb41ce3dde955c38c9df3262f3712e640
wkl--007 <499657357@qq.com>
2018-05-27 16:36:59 +0800

    头部完成

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import 'moment/locale/zh-cn'
 import groupBy from 'lodash/groupBy'
 import Debounce from 'lodash-decorators/debounce'
 import { Menu, Icon, Spin, Tag, Dropdown, Avatar, Divider, Tooltip } from 'antd'
@@ -9,112 +10,52 @@ import HeaderSearch from 'ant-design-pro/lib/HeaderSearch'
 
 import styles from './index.less'
 
-const noticeData = [
-  {
-    id: '000000001',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
-    title: '你收到了 14 份新周报',
-    datetime: '2017-08-09',
-    type: '通知',
-  }, {
-    id: '000000002',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png',
-    title: '你推荐的 曲妮妮 已通过第三轮面试',
-    datetime: '2017-08-08',
-    type: '通知',
-  }, {
-    id: '000000003',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png',
-    title: '这种模板可以区分多种通知类型',
-    datetime: '2017-08-07',
-    read: true,
-    type: '通知',
-  }, {
-    id: '000000004',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
-    title: '左侧图标用于区分不同的类型',
-    datetime: '2017-08-07',
-    type: '通知',
-  }, {
-    id: '000000005',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
-    title: '内容不要超过两行字，超出时自动截断',
-    datetime: '2017-08-07',
-    type: '通知',
-  }, {
-    id: '000000006',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-    title: '曲丽丽 评论了你',
-    description: '描述信息描述信息描述信息',
-    datetime: '2017-08-07',
-    type: '消息',
-  }, {
-    id: '000000007',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-    title: '朱偏右 回复了你',
-    description: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像',
-    datetime: '2017-08-07',
-    type: '消息',
-  }, {
-    id: '000000008',
-    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
-    title: '标题',
-    description: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像',
-    datetime: '2017-08-07',
-    type: '消息',
-  }, {
-    id: '000000009',
-    title: '任务名称',
-    description: '任务需要在 2017-01-12 20:00 前启动',
-    extra: '未开始',
-    status: 'todo',
-    type: '待办',
-  }, {
-    id: '000000010',
-    title: '第三方紧急代码变更',
-    description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
-    extra: '马上到期',
-    status: 'urgent',
-    type: '待办',
-  }, {
-    id: '000000011',
-    title: '信息安全考试',
-    description: '指派竹尔于 2017-01-09 前完成更新并发布',
-    extra: '已耗时 8 天',
-    status: 'doing',
-    type: '待办',
-  }, {
-    id: '000000012',
-    title: 'ABCD 版本发布',
-    description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
-    extra: '进行中',
-    status: 'processing',
-    type: '待办',
-  }]
-
 class GlobalHeader extends PureComponent {
   constructor (props) {
     super(props)
-    this.state = {
-      currentUser: {
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
-        name: '小王',
-        notifyCount: 12,
-        userid: '00000001',
-      },
-    }
   }
 
   componentWillUnmount () {
     this.triggerResizeEvent.cancel()
   }
 
-  getNoticeData = () => {
-
+  //获取notice列表
+  getNoticeData = (notices) => {
+    if (notices.length === 0) {
+      return {}
+    }
+    const newNotices = notices.map(notice => {
+      const newNotice = {...notice}
+      if (newNotice.datetime) {
+        newNotice.datetime = moment(notice.datetime).fromNow()
+      }
+      // transform id to item key
+      if (newNotice.id) {
+        newNotice.key = newNotice.id
+      }
+      if (newNotice.extra && newNotice.status) {
+        const color = {
+          todo: '',
+          processing: 'blue',
+          urgent: 'red',
+          doing: 'gold',
+        }[newNotice.status]
+        newNotice.extra = (
+          <Tag color={color} style={{marginRight: 0}}>
+            {newNotice.extra}
+          </Tag>
+        )
+      }
+      return newNotice
+    })
+    return groupBy(newNotices, 'type')
   }
 
+  //左侧菜单展开收缩
   toggle = () => {
-
+    let {collapsed, onCollapse} = this.props
+    onCollapse(!collapsed)
+    this.triggerResizeEvent()
   }
 
   @Debounce(600)
@@ -131,6 +72,7 @@ class GlobalHeader extends PureComponent {
       collpased,
       fetchingNotices,
       isMobile,
+      notices,
       logo,
       onNoticeVisibleChange,
       onMenuClick,
@@ -152,6 +94,7 @@ class GlobalHeader extends PureComponent {
         </Menu.Item>
       </Menu>
     )
+    let noticeData = this.getNoticeData(notices)
     return (
       <div className={styles.header}>
         {
--- src/layouts/basicLayout.js
+++ src/layouts/basicLayout.js
@@ -12,6 +12,8 @@ import PropTypes from 'prop-types'
 import { getMenuData } from '../router/menu'
 import { getRoutes } from '../utils/utils'
 
+import logo from '../assets/images/logo.svg'
+
 const {Content, Header, Footer} = Layout
 const query = {
   'screen-xs': {
@@ -57,6 +59,116 @@ const getRedirect = item => {
 getMenuData().forEach(getRedirect)
 
 class basicLayout extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentUser: {
+        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+        name: '小王',
+        notifyCount: 12,
+        userid: '00000001',
+      },
+      isMobile: isMobile,
+      fetchingNotices: false,
+
+      collapsed: false,//菜单展开收缩，需要放在redux中
+      notices: [
+        {
+          id: '000000001',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+          title: '你收到了 14 份新周报',
+          datetime: '2017-08-09',
+          type: '通知',
+        }, {
+          id: '000000002',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/OKJXDXrmkNshAMvwtvhu.png',
+          title: '你推荐的 曲妮妮 已通过第三轮面试',
+          datetime: '2017-08-08',
+          type: '通知',
+        }, {
+          id: '000000003',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/kISTdvpyTAhtGxpovNWd.png',
+          title: '这种模板可以区分多种通知类型',
+          datetime: '2017-08-07',
+          read: true,
+          type: '通知',
+        }, {
+          id: '000000004',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/GvqBnKhFgObvnSGkDsje.png',
+          title: '左侧图标用于区分不同的类型',
+          datetime: '2017-08-07',
+          type: '通知',
+        }, {
+          id: '000000005',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/ThXAXghbEsBCCSDihZxY.png',
+          title: '内容不要超过两行字，超出时自动截断',
+          datetime: '2017-08-07',
+          type: '通知',
+        }, {
+          id: '000000006',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+          title: '曲丽丽 评论了你',
+          description: '描述信息描述信息描述信息',
+          datetime: '2017-08-07',
+          type: '消息',
+        }, {
+          id: '000000007',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+          title: '朱偏右 回复了你',
+          description: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像',
+          datetime: '2017-08-07',
+          type: '消息',
+        }, {
+          id: '000000008',
+          avatar: 'https://gw.alipayobjects.com/zos/rmsportal/fcHMVNCjPOsbUGdEduuv.jpeg',
+          title: '标题',
+          description: '这种模板用于提醒谁与你发生了互动，左侧放『谁』的头像',
+          datetime: '2017-08-07',
+          type: '消息',
+        }, {
+          id: '000000009',
+          title: '任务名称',
+          description: '任务需要在 2017-01-12 20:00 前启动',
+          extra: '未开始',
+          status: 'todo',
+          type: '待办',
+        }, {
+          id: '000000010',
+          title: '第三方紧急代码变更',
+          description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
+          extra: '马上到期',
+          status: 'urgent',
+          type: '待办',
+        }, {
+          id: '000000011',
+          title: '信息安全考试',
+          description: '指派竹尔于 2017-01-09 前完成更新并发布',
+          extra: '已耗时 8 天',
+          status: 'doing',
+          type: '待办',
+        }, {
+          id: '000000012',
+          title: 'ABCD 版本发布',
+          description: '冠霖提交于 2017-01-06，需在 2017-01-07 前完成代码变更任务',
+          extra: '进行中',
+          status: 'processing',
+          type: '待办',
+        }],//注意消息
+    }
+  }
+
+  componentDidMount () {
+    this.enquireHandler = enquireScreen(mobile => {
+      this.setState({
+        isMobile: mobile,
+      })
+    })
+  }
+
+  componentWillUnmount () {
+    unenquireScreen(this.enquireHandler)
+  }
+
   //获取页面标题
   getPageTitle = () => {
     let {routerData, location} = this.props
@@ -74,16 +186,77 @@ class basicLayout extends Component {
     return title
   }
 
+  //消息清除
+  handleNoticeClear = (tabTitle) => {
+    let {notices, currentUser} = this.state
+    let newNotice = []
+    notices.map(notice => {
+      if (notice.type !== tabTitle) {
+        newNotice.push(notice)
+      }
+    })
+    let data = Object.assign({}, currentUser, {notifyCount: newNotice.length})
+    this.setState({
+      notices: newNotice,
+      currentUser: data,
+    })
+    message.success(`清空了${tabTitle}`)
+  }
+
+  //控制menu收缩展开
+  handleMenuCollapse = (collapsed) => {
+    this.setState({
+      collapsed: collapsed,
+    })
+  }
+
+  //
+  handleMenuClick = ({key}) => {
+    if (key === 'triggerError') {
+      console.log('触发报错')
+    } else if (key === 'logout') {
+      console.log('退出登录')
+    }
+  }
+
+  //
+  handleNoticeVisibleChange = (visible) => {
+    if (visible) {
+      this.setState({
+        fetchingNotices:true
+      })
+      setTimeout(()=>{
+        this.setState({
+          fetchingNotices:false
+        })
+      },200)
+      console.log(visible)
+    }
+  }
+
   render () {
     let {match, routerData} = this.props
+    let {currentUser, isMobile, fetchingNotices, collapsed, notices} = this.state
     let routes = getRoutes(match.path, routerData)
     let bashRedirect = routes[0].path
     let layout = (
       <Layout>
         <Layout>
           <Header style={{padding: 0}}>
-
+            <GlobalHeader
+              logo={logo}
+              currentUser={currentUser}
+              isMobile={isMobile}
+              fetchingNotices={fetchingNotices}
+              notices={notices}
+              collapsed={collapsed}
+              onNoticeClear={this.handleNoticeClear}
+              onCollapse={this.handleMenuCollapse}
+              onMenuClick={this.handleMenuClick}
+              onNoticeVisibleChange={this.handleNoticeVisibleChange}
+            />
           </Header>
+
         </Layout>
       </Layout>
     )
@@ -92,8 +265,6 @@ class basicLayout extends Component {
       <DocumentTitle title={this.getPageTitle()}>
         <ContainerQuery query={query}>
           {params => <div className={classNames(params)}>{layout}</div>}
-
-
           {/*<div id="basic-layout">
 
             <Switch>
