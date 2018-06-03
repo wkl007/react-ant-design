39a5a93fdcaf13c3f3c802029a9eb25ff752b249
wkl--007 <499657357@qq.com>
2018-06-03 13:18:15 +0800

    查询列表完成

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux'
 import configureStore from './redux/store/configureStore'
 import registerServiceWorker from './registerServiceWorker'
 import 'moment/locale/zh-cn'
+import zhCN from 'antd/lib/locale-provider/zh_CN'
+import { LocaleProvider } from 'antd'
 
 import './index.css'
 import 'nprogress/nprogress.css'
@@ -17,9 +19,11 @@ const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
-      <App/>
-    </HashRouter>
+    <LocaleProvider locale={zhCN}>
+      <HashRouter>
+        <App/>
+      </HashRouter>
+    </LocaleProvider>
   </Provider>,
   document.getElementById('root'))
 registerServiceWorker()
